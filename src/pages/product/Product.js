3a7d2e7e115ef3665a681e8d0e99c0db759c2154
souad988 <59707859+souad988@users.noreply.github.com>
2022-04-27 18:55:06 +0000

    Add product rendering condition

--- src/pages/product/Product.js
+++ src/pages/product/Product.js
@@ -5,23 +5,35 @@ import style from './product.module.css';
 import ProductInfo from '../../components/product/ProductInfo';
 
 function Product() {
-  const product = useSelector(state=> state)
-  console.log('from product',product)
+  const product = useSelector((state) => state);
+  console.log('from product', product);
   return (
+    <div>
+    {product?
     <div className={style.productContainer}>
-       <section className={style.productSection}>
-         <img src={product.picture} alt='product img' />
-         <div><span> {product.name} </span><span>{product.type.name}</span></div>
-         <ProductInfo description={product.description} />
-       </section>
-       <section >
-         <div className={style.userContainer}>
-        <UserInfo name = {[product.user.firstName,product.user.lastName].join(' ')}
-                  imgUrl = {product.user.profilePicture}
-                  companyName = {product.company.name}
-         />
-         </div>
-        </section>
+      <section className={style.productSection}>
+        <img src={product.picture} alt="product img" />
+        <div>
+          <span>
+            {' '}
+            {product.name}
+            {' '}
+          </span>
+          <span>{product.type.name}</span>
+        </div>
+        <ProductInfo description={product.description} />
+      </section>
+      <section>
+        <div className={style.userContainer}>
+          <UserInfo
+            name={[product.user.firstName, product.user.lastName].join(' ')}
+            imgUrl={product.user.profilePicture}
+            companyName={product.company.name}
+          />
+        </div>
+      </section>
+    </div>
+    : ''}
     </div>
   );
 }
